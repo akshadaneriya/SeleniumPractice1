c7e8b1c39b24f757a9460c3981843826abc7c809
akshadaneriya1 <akshada.neriya@example.com>
2022-09-05 22:02:35 +0530

    updated files

--- SeleniumWithJava/src/PropertyFile/GetDataFromPropertyFile.java
+++ SeleniumWithJava/src/PropertyFile/GetDataFromPropertyFile.java
@@ -3,8 +3,7 @@ package PropertyFile;
 public class GetDataFromPropertyFile {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("My text");
 	}
 
 }
